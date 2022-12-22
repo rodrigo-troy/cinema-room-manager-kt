857b66db03a99b3d109d7b8e66138d59896667d8
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2022-12-22 13:53:41 -0300

    Stage 4

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -5,29 +5,53 @@ fun main() {
 }
 
 fun cinemaRoom() {
+
     println("Enter the number of rows:")
     val rows = readln().toInt()
     println("Enter the number of seats in each row:")
     val seats = readln().toInt()
+    println()
 
-    printSeats(rows, seats)
+    val cinema = Array(rows) { CharArray(seats) { 'S' } };
 
+    while (true) {
+        println("1. Show the seats")
+        println("2. Buy a ticket")
+        println("0. Exit")
+
+        when (readln().toInt()) {
+            1 -> printSeats(cinema)
+            2 -> buyTicket(cinema)
+            0 -> return
+        }
+    }
+}
+
+fun buyTicket(cinema: Array<CharArray>) {
+    println()
     println("Enter a row number:")
     val rowNumber = readln().toInt()
     println("Enter a seat number in that row:")
     val seatNumber = readln().toInt()
 
-    println("\nTicket price: $${getTicketPrice(rows, seats, rowNumber - 1)}")
-    printSeatsWithReservation(rows, seats, rowNumber - 1, seatNumber - 1)
+    if (cinema[rowNumber - 1][seatNumber - 1] == 'B') {
+        println("That ticket has already been purchased!")
+        return
+    } else {
+        cinema[rowNumber - 1][seatNumber - 1] = 'B'
+    }
+
+    println("Ticket price: $${getTicketPrice(cinema, rowNumber)}")
+    println()
 }
 
-fun getTicketPrice(rows: Int, seats: Int, rowIndex: Int): Int {
-    val totalSeats = rows * seats
+fun getTicketPrice(cinema: Array<CharArray>, rowIndex: Int): Int {
+    val totalSeats = cinema.size * cinema[0].size
 
     return if (totalSeats <= 60) {
         10
     } else {
-        val frontRows = rows / 2
+        val frontRows = cinema.size / 2
 
         if (rowIndex < frontRows) {
             10
@@ -37,29 +61,24 @@ fun getTicketPrice(rows: Int, seats: Int, rowIndex: Int): Int {
     }
 }
 
-fun printSeats(rows: Int, seats: Int) {
-    print("\nCinema:\n  ")
-    (1..seats).forEach { print("$it ") }
+fun printSeats(cinema: Array<CharArray>) {
     println()
-    (1..rows).forEach { print("$it ${"S ".repeat(seats)}\n") }
-    println()
-}
+    print("Cinema:\n  ")
+    for (i in 1..cinema[0].size) {
+        print("$i ")
+    }
 
-fun printSeatsWithReservation(rows: Int, seats: Int, rowIndex: Int, seatIndex: Int) {
-    print("\nCinema:\n  ")
-    (1..seats).forEach { print("$it ") }
     println()
-    (1..rows).forEach {
-        print("$it ")
-        (1..seats).forEach { seat ->
-            if (it == rowIndex + 1 && seat == seatIndex + 1) {
-                print("B ")
-            } else {
-                print("S ")
-            }
+
+    for (i in cinema.indices) {
+        print("${i + 1} ")
+        for (j in cinema[i].indices) {
+            print("${cinema[i][j]} ")
         }
         println()
     }
+
+    println()
 }
 
 fun printProfit(rows: Int, seats: Int) {
